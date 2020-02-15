e71767b1e4209d7bbc329c76b72a9361b6a31bee
Austin Weigel <db5342jj@metrostate.edu>
2020-02-14 20:56:44 -0600

    Update to focus lock logic

--- Raspberry/focuscontrol.py
+++ Raspberry/focuscontrol.py
@@ -2,12 +2,14 @@ import threading
 
 lock = threading.Lock()
 
-def boolean adjust_focus(amount):
+#If the focus is being used returns failure notification, if not aquires lock and adjusts amount
+def adjust_focus(amount):
     if not lock.acquire(False):
         return  'The focus motor is adjusting. This adjustment will not be processed.'
     else:
         try:
             #put focus adjustment code here
+            return True
         finally:
             lock.release()
             return 'The focus has been adjusted by ' + str(amount) + 'successfully'
